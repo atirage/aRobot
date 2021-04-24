a19d2af60b48cc7100b4b6dfa0762a9f4c8bfa89
atirage <attila@csibi.com>
2021-04-24 23:57:39 +0200

    Added camera stream to GUI

--- aRobot.py
+++ aRobot.py
@@ -5,13 +5,19 @@
 #import machine
 #import aHBridge_ESP as dualHbr
 
+import time
 import socket
 import asyncio
 import json
 import aHBridge_PCA as dualHbr
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
-server = '192.168.0.101'
-port = 65432
+SERVER = '192.168.0.61'
+PORT   = 65432
+WIDTH  = 320
+HEIGHT = 240
+DEPTH  = 3
 
 async def heartbeat(tms):
     #led = machine.Pin(2, machine.Pin.OUT, value=1)
@@ -20,53 +26,17 @@ async def heartbeat(tms):
         #await asyncio.sleep_ms(tms)
         await asyncio.sleep(tms / 1000)
 
-async def listen(robo):
-    #sock = socket.socket()
-    try:
-        #serv = socket.getaddrinfo(server, port)[0][-1]
-        #sock.connect(serv)
-        sreader, swriter = await asyncio.open_connection(server, port)
-    except OSError:
-        print('Cannot connect to {} on port {}'.format(server, port))
-        #sock.close()
-        return
-    #swriter = asyncio.StreamWriter(sock, {})
-    #await swriter.awrite('{}\n'.format(json.dumps(['ready', 1])))
-    rsp = '{}\n'.format(json.dumps(['ready', 1]))
-    swriter.write(rsp.encode())
-    while True:
-        #sreader = asyncio.StreamReader(sock)
-        while True:
-            try:
-                res = await sreader.readline()
-            except OSError:
-                swriter.close()
-                #await writer.wait_closed()
-                return
-            try:
-                cmd = json.loads(res)
-                s = cmd["Speed"]
-                l = r = 50
-                if cmd["Angle"] <= 0:
-                    #turn left
-                    l = 50 + cmd["Angle"] 
-                    r = 50
-                else:
-                    #turn right
-                    l = 50
-                    r = 50 - cmd["Angle"]                       
-                robo.remote_ctrl((s * l),
-                                 (s * r))
-            except ValueError:
-                #sock.close()
-                swriter.close()
-                #await writer.wait_closed()
-                return
-            #await asyncio.sleep_ms(20)
-            await asyncio.sleep(0.02)
-
 class aRobot:
-    def __init__(self):
+    def __init__(self, loop):
+        self.loop = loop
+
+        self.cam = PiCamera()
+        self.cam.resolution = (WIDTH, HEIGHT)
+        self.cam.framerate = 24
+
+        self.sreader = []
+        self.swriter = []
+
         self.behaviors = [
             {
                 "name":"ESCAPE",
@@ -99,8 +69,8 @@ class aRobot:
         ]
         self.Vbat = 9.6
         self.dc = [0, 0]
-        #self.dhb = dualHbr.aDualHBridge(14, 0, 4, 5, 1000)
-        self.dhb = dualHbr.aDualHBridge(0, 1, 2, 3, 1000)
+        ##self.dhb = dualHbr.aDualHBridge(14, 0, 4, 5, 1000)
+        #self.dhb = dualHbr.aDualHBridge(0, 1, 2, 3, 1000)
 
     def remote_ctrl(self, left, right):
         #print(left, right)
@@ -118,27 +88,78 @@ class aRobot:
     
     def execute(self, dc):
         if self.dc[0] != dc[0]:
-            self.dhb.duty_m1(self.dc[0] / 5000)
+            #self.dhb.duty_m1(self.dc[0] / 5000)
             self.dc[0] = dc[0]
 
         if self.dc[1] != dc[1]:
-            self.dhb.duty_m2(self.dc[1] / 5000)
+            #self.dhb.duty_m2(self.dc[1] / 5000)
             self.dc[1] = dc[1]
 
+    async def register(self):
+        try:
+            #serv = socket.getaddrinfo(server, port)[0][-1]
+            #sock.connect(serv)
+            self.sreader, self.swriter = await asyncio.open_connection(SERVER, PORT)
+            rsp = '{}\n'.format(json.dumps(['ready', 1]))
+            self.swriter.write(rsp.encode())
+        except OSError:
+            print('Cannot connect to {} on port {}'.format(SERVER, PORT))
+            #sock.close()
+
+    async def listen(self):
+        while True:
+            try:
+                res = await self.sreader.readline()
+                cmd = json.loads(res)
+                s = cmd["Speed"]
+                l = r = 50
+                if cmd["Angle"] <= 0:
+                    #turn left
+                    l = 50 + cmd["Angle"] 
+                    r = 50
+                else:
+                    #turn right
+                    l = 50
+                    r = 50 - cmd["Angle"]                       
+                self.remote_ctrl((s * l), (s * r))
+            except (ValueError, OSError):
+                #swriter.close()
+                #await writer.wait_closed()
+                return
+            await asyncio.sleep(0.02)
+
+    async def detect(self):
+        raw = PiRGBArray(self.cam, size=(WIDTH, HEIGHT))
+        for frame in self.cam.capture_continuous(raw, format="bgr", use_video_port=True):
+            # grab the raw NumPy array representing the image, then initialize the timestamp
+            # and occupied/unoccupied text
+            img = frame.array
+            #print(img.dtype, img.shape)
+            self.swriter.write(img.tobytes())
+            await self.swriter.drain()
+            await asyncio.sleep(0.04)
+            # clear the stream in preparation for the next frame
+            raw.truncate(0)
+    
     async def run(self):
+        await self.register()
+        self.loop.create_task(self.listen())
+        self.loop.create_task(self.detect())
         while True:
             self.Vbat = 6.0#machine.ADC(0).read() * 11 / 1024
             #self.remote_ctrl(512, 512) #pwm range 0:1024
             self.execute(dc = self.arbitrate())
-            #await asyncio.sleep_ms(50)
             await asyncio.sleep(0.05)
 
 def start():
-    robo = aRobot()
     loop = asyncio.get_event_loop()
+    robo = aRobot(loop)
+    time.sleep(0.1)
     loop.create_task(heartbeat(200)) # Optional fast heartbeat to confirm nonblocking operation
-    loop.create_task(listen(robo))
+
     try:
         loop.run_until_complete(robo.run())
     except KeyboardInterrupt:
+        #robo.swriter.close()
+        #robo.sreader.close()
         print('Interrupted')  # This mechanism doesn't work on Unix build.
--- aRobotSrvr.py
+++ aRobotSrvr.py
@@ -5,58 +5,115 @@ import tkinter as tk
 import socket
 import json
 import time
+import PIL.Image
+import PIL.ImageTk
+import numpy as np
+import timeit
 
-HOST = '192.168.0.101'
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST   = '192.168.0.61'#'localhost'
+PORT   = 65432        # Port to listen on (non-privileged ports are > 1023)
+WIDTH  = 320
+HEIGHT = 240
+DEPTH  = 3
 
 cmd =  { "Speed": 0, "Angle": 0}
 stop_thr = False
+
+conn = []
 
 #def sel():
 #   selection = "Value = " + str(var.get())
 #   label.config(text = selection)
 
-def handleConnection():
-    ready = False
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        print('Listening...')
-        s.listen()
-        conn, addr = s.accept()
-        print('Connected by ', addr)
-        with conn:
-            while True:
-                if stop_thr:
-                    break
-                if not ready:
-                    # wait for client readiness
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    x = json.loads(data)
-                    ready = x[0]
-                else:
-                    #poll for values
-                    cmd["Speed"] = scaleS.get()
-                    cmd["Angle"] = scaleA.get()
-                    x = '{}\n'.format(json.dumps(cmd))
-                    conn.sendall(x.encode())
-                    time.sleep(0.05)
+def waitConnection(ev, text):
+    global conn
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    text.insert(tk.END, "Listening...\n")
+    s.listen()
+    conn, addr = s.accept()
+    text.insert(tk.END, "Connected by " + str(addr))
+    # wait for client readiness
+    data = conn.recv(1024)
+    if not data:
+        text.insert(tk.END, "Invalid data!")
+        return
+    x = json.loads(data)
+    if x[0]:
+        ev.set()
+                    
+def sendCommands(ev):
+    global conn
+    ev.wait()
+    while not stop_thr:
+        #poll for values
+        cmd["Speed"] = scaleS.get()
+        cmd["Angle"] = scaleA.get()
+        x = '{}\n'.format(json.dumps(cmd))
+        conn.sendall(x.encode())
+        time.sleep(0.05)
 
-thr = threading.Thread(target=handleConnection, args=())
+def recvFrames(ev, label):
+    global conn
+    ev.wait()
+    size = 0
+    data = b""
+    #start_time = timeit.default_timer()
+    while not stop_thr:
+        chunk = conn.recv(4096)
+        l = len(chunk)
+        #deserialize after HEIGHT * WIDTH * DEPTH bytes
+        if size < (HEIGHT * WIDTH * DEPTH - l):
+            data = b"".join([data, chunk])
+            size += l
+        else:
+            #start_time = timeit.default_timer()
+            data = b"".join([data, chunk[:HEIGHT * WIDTH * DEPTH - size]])
+            cv_img = np.frombuffer(data, dtype=np.uint8).reshape((HEIGHT, WIDTH, DEPTH))
+            if l > HEIGHT * WIDTH * DEPTH - size:
+                data = chunk[HEIGHT * WIDTH * DEPTH - size:]
+            else:
+                data = b""   
+            size = len(data)
+            photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_img))
+            label.configure(image=photo)
+            label.image = photo
+            #print(timeit.default_timer() - start_time)
+            #start_time = timeit.default_timer()
+
 top = tk.Tk()
-top.geometry("200x180") 
+top.title("aRobot GUI")
+top.geometry("440x320") 
 #var = DoubleVar()
-scaleS = tk.Scale(top, from_ = 100, to = -100, label = 'Speed', orient = tk.VERTICAL)#, variable = var )
-scaleA = tk.Scale(top, from_ = -50, to = 50, label = 'Steering', orient = tk.HORIZONTAL)
+labelR = tk.Label(top, width = 320, height = 240)
+canvasL = tk.Canvas(top, width = 120, height = 240)
+canvasB = tk.Canvas(top, width = 440, height = 80)
+#create widgets
+scaleS = tk.Scale(canvasL, from_ = 100, to = -100, label = 'Speed', orient = tk.VERTICAL)#, variable = var )
+scaleA = tk.Scale(canvasL, from_ = -50, to = 50, label = 'Steering', orient = tk.HORIZONTAL)
+text   = tk.Text(canvasB, height = 4, width = 60)
+#place widgets
+canvasB.pack(side = tk.BOTTOM)
+canvasL.pack(side = tk.LEFT)
+labelR.pack(side = tk.RIGHT)
 scaleS.pack(side = tk.TOP)
 scaleA.pack(side = tk.BOTTOM)
+text.pack()
+
+ev = threading.Event()
+wait_conn = threading.Thread(target=waitConnection, args=(ev, text,))
+send_cmds = threading.Thread(target=sendCommands, args=(ev,))
+recv_frms = threading.Thread(target=recvFrames, args=(ev, labelR,))
+
 #button = Button(root, text = "Send", command = sel)
 #button.pack(anchor = CENTER)
 #label = Label(root)
 #label.pack()
 
-thr.start()
+wait_conn.start()
+send_cmds.start()
+recv_frms.start()
 top.mainloop()
 stop_thr = True
-thr.join()          
+send_cmds.join()  
+recv_frms.join() 
